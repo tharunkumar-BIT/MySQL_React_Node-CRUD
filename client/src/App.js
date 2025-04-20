91d5763c70b59117da2b7ee08657bd50a9b16ac5
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-20 18:27:25 +0530

    create record function

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,83 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [salary, setSalary] = useState("");
+
+
+  const addEmployee = () => {
+    axios
+      .post("http://localhost:3001/create", {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        salary: salary,
+      })
+      .then(() => {
+        console.log("Successfully added employee");
+      });
+  };
+
   return (
     <div className="App">
-      
+      <div className="info">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          type="number"
+          name="age"
+          id="age"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <label htmlFor="country">Country</label>
+        <input
+          type="text"
+          name="country"
+          id="country"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
+        <label htmlFor="position">Position</label>
+        <input
+          type="text"
+          name="position"
+          id="position"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <label htmlFor="salary">Salary(year)</label>
+        <input
+          type="number"
+          name="salary"
+          id="salary"
+          onChange={(e) => {
+            setSalary(e.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add employee</button>
+      </div>
+
+      <div className="employees">
+        <button>Show Employees</button>
+      </div>
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+const port = 3001;
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "employeesystem",
+});
+
+app.post("/create", (req, res) => {
+  const { name, age, country, position, salary } = req.body;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, salary) VALUES(?,?,?,?,?)",
+    [name, age, country, position, salary],
+    (err) => {
+      if (err) console.log(err);
+      else res.send("Values Inserted");
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log("Server running on :" + port);
+});
